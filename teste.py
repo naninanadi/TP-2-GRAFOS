d29599db8e7242b2b7f4749688d554a6d618ea5a
Ana Clara <nanakkj.sillaby@gmail.com>
2025-11-06 00:04:37 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import gcol
-
-G = nx.dodecahedral_graph()
-c = gcol.node_coloring(G)
-print("Here is a node coloring of graph G:", c)
-nx.draw_networkx(G, node_color=gcol.get_node_colors(G, c))
-plt.show()
